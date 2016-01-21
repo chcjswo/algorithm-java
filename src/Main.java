6d1b631596f24cfa197b980365520e4691523c59
chcjswo <chcjswo@gmail.com>
2016-01-21 16:26:35 +0900

    공부중
    팩토리얼 0개수 맞히기 성공

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
         
-        String s = sc.nextLine();
-        int[] alpa = new int[26];
-        
-        for (int i = 0; i < s.length(); i++) {
-        	int x = (int)s.substring(i, i+1).charAt(0);
-        	for (int j=97; j<=122; j++) {
-        		if (x == j) {
-        			alpa[j - 97] = alpa[j - 97] + 1;
-        		}
-        	}
-		}
-        
-        for (int i : alpa) {
-        	System.out.print(i + " ");
-        }
+        System.out.println(a);
     }
+
 }
---
+++ src/fc/algoritm/math/Factorial_03.java
@@ -0,0 +1,18 @@
+package fc.algoritm.math;
+
+import java.util.Scanner;
+
+public class Factorial_03 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int sum = 0;
+        
+        for (int i=5; i<=a; i*=5) {
+        	sum += a/i;
+        }
+        
+        System.out.println(sum);
+    }
+    
+}
