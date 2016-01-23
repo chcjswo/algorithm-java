62fd4de722329bbaaee1ec41c8719cdc5ea02301
chcjswo <chcjswo@gmail.com>
2016-01-24 00:45:58 +0900

    풀이
    ROT13 성공

--- src/fc/algoritm/datastructure/Rot13.java
+++ src/fc/algoritm/datastructure/Rot13.java
@@ -1,6 +1,8 @@
+package fc.algoritm.datastructure;
+
 import java.util.Scanner;
 
-public class Main {
+public class Rot13 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
