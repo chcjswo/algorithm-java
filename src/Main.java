838deedfed89a3397428543695f1fbcc2c3144a9
chcjswo <chcjswo@gmail.com>
2016-01-23 18:42:02 +0900

    풀이중
    안되니까 짜증나네

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
-    	
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         
-        while (sc.hasNext()) {
-	        String s = sc.nextLine();
-			System.out.println(s);
+        for (int i=1; i <=n; i++) {
+        	int count = n-i;
+        	int loop = 1;
+        	for (int j=1; j<=n*2-1; j++) {
+        		if (count > 0) {
+        			System.out.print(" ");
+        			count--;
+        		} else if (count == 0) {
+        			System.out.print("*");
+        			count = n-i; 
+        		} else {
+        			System.out.print("*");
+        			count--; 
+        		}
+        	}
+        	System.out.println("");
         }
     }
 
