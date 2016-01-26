769e4078dbdd915399c35a46d8459896b297a69f
chcjswo <chcjswo@gmail.com>
2016-01-27 01:07:30 +0900

    풀이
    dfs bfs 풀이
    내가 한건 아니고...

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,64 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int start = sc.nextInt();
+		List<Integer>[] a = (List<Integer>[]) new List[n+1];
+        for (int i=1; i<=n; i++) {
+            a[i] = new ArrayList<Integer>();
+        }
+        for (int i=0; i<m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            a[u].add(v);
+            a[v].add(u);
+        }
+        for (int i=1; i<=n; i++) {
+            Collections.sort(a[i]);
+        }
+        boolean[] c = new boolean[n+1];
+        dfs(a, c, start);
+        System.out.println();
+        Arrays.fill(c, false);
+        bfs(a, c, start);
+        System.out.println();
+    }
 
-		int n = sc.nextInt();
-		int mod = 2;
-		n = Integer.parseInt(String.valueOf(n), 8);
-		
-		ArrayList<Integer> data = new ArrayList<Integer>();
-		
-		while (n != 0) {
-			data.add(n%mod);
-			n = n / mod;
-		}
-		
-		for (int i=data.size()-1; i>=0; i--) {
-			System.out.print(data.get(i));
-		}
-	}
-
+    public static void dfs(List<Integer>[] a, boolean[] c, int x) {
+        if (c[x]) {
+            return;
+        }
+        c[x] = true;
+        System.out.print(x + " ");
+        for (int y : a[x]) {
+            if (c[y] == false) {
+                dfs(a, c, y);
+            }
+        }
+    }
+    
+    public static void bfs(List<Integer>[] a, boolean[] c, int start) {
+        Queue<Integer> q = new LinkedList<Integer>();
+        q.add(start);
+        c[start] = true;
+        while (!q.isEmpty()) {
+            int x = q.remove();
+            System.out.print(x + " ");
+            for (int y : a[x]) {
+                if (c[y] == false) {
+                    c[y] = true;
+                    q.add(y);
+                }
+            }
+        }
+    }
 }
