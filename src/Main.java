1726242b41d6facb8153fd5d05caa31a814f9c66
chcjswo <chcjswo@gmail.com>
2016-01-29 18:13:51 +0900

    풀이
    쇠막대기 성공

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,33 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int start = sc.nextInt();
-		List<Integer>[] a = (List<Integer>[]) new List[n+1];
-        for (int i=1; i<=n; i++) {
-            a[i] = new ArrayList<Integer>();
-        }
-        for (int i=0; i<m; i++) {
-            int u = sc.nextInt();
-            int v = sc.nextInt();
-            a[u].add(v);
-            a[v].add(u);
-        }
-        for (int i=1; i<=n; i++) {
-            Collections.sort(a[i]);
-        }
-        boolean[] c = new boolean[n+1];
-        dfs(a, c, start);
-        System.out.println();
-        Arrays.fill(c, false);
-        bfs(a, c, start);
-        System.out.println();
-    }
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.nextLine();
+		int size = s.length();
+		Stack<Integer> stack = new Stack<Integer>();
+		int sum = 0;
+		
+		for (int i = 0; i < size; i++) {
+			char x = s.charAt(i);
+			
+			if (x == '(') {
+				stack.push(i);
+			} else {
+				if (stack.peek()+1 == i) {
+					stack.pop();
+					sum += stack.size();
+				} else {
+					stack.pop();
+					sum++;
+				}
+			}
+		}
+		
+		System.out.println(sum);
 
-    public static void dfs(List<Integer>[] a, boolean[] c, int x) {
-        if (c[x]) {
-            return;
-        }
-        c[x] = true;
-        System.out.print(x + " ");
-        for (int y : a[x]) {
-            if (c[y] == false) {
-                dfs(a, c, y);
-            }
-        }
-    }
-    
-    public static void bfs(List<Integer>[] a, boolean[] c, int start) {
-        Queue<Integer> q = new LinkedList<Integer>();
-        q.add(start);
-        c[start] = true;
-        while (!q.isEmpty()) {
-            int x = q.remove();
-            System.out.print(x + " ");
-            for (int y : a[x]) {
-                if (c[y] == false) {
-                    c[y] = true;
-                    q.add(y);
-                }
-            }
-        }
-    }
+	}
+
 }
