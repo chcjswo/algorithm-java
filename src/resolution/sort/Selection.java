21e6640ddd61bf06ab3b85e8494d6cff24c50707
tony <chcjswo@gmail.com>
2019-06-12 18:52:07 +0900

    선택 정렬 연습

---
+++ src/resolution/sort/Selection.java
@@ -0,0 +1,19 @@
+package resolution.sort;
+
+import java.util.Scanner;
+
+/**
+ * 선택 정렬
+ * Created by chcjswo on 2019-06-12
+ * Blog: http://mocadev.tistory.com
+ * Github: http://github.com/chcjswo
+ */
+public class Selection {
+    public static void main(String[] args) {
+        int[] data = {1, 2, 10, 4, 6, 7, 3, 11, 5, 23, 22, 224, 356};
+
+        for (int item : data) {
+            System.out.print(item + " ");
+        }
+    }
+}
