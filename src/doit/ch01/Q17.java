05bc7ba0c2aaeac4f6022fbbf47649d4ce65abfc
tony <chcjswo@gmail.com>
2019-06-14 00:08:21 +0900

    Do it! Q17 문제 풀이

--- src/doit/ch01/Q17.java
+++ src/doit/ch01/Q17.java
@@ -3,26 +3,26 @@ package doit.ch01;
 import java.util.Scanner;
 
 /**
- * Do it! Q16 문제
+ * Do it! Q17 문제
  * Created by chcjswo on 2019-06-13
  * Blog: http://mocadev.tistory.com
  * Github: http://github.com/chcjswo
  */
-public class Q16 {
+public class Q17 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        star(n);
+        numberPyramid(n);
     }
 
-    private static void star(int n) {
-        for (int i = 0; i < n; i++) {
-            int max = i * 2 + 1;
-            for (int j = 0; j < n - (i + 1); j++) {
+    private static void numberPyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            int max = (i - 1) * 2 + 1;
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 0; j < max; j++) {
-                System.out.print( "*");
+            for (int j = 1; j <= max; j++) {
+                System.out.print(i);
             }
             System.out.println();
 
