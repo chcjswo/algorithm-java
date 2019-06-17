e9a85fcc38cf083ed704fd0b05d6414ab54923b2
tony <chcjswo@gmail.com>
2019-06-17 13:02:35 +0900

    백준 15890 풀이중

---
+++ src/baekjoon/Question_15890.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * 백준 15890번 문제
+ * Created by chcjswo on 2019-06-17
+ * Blog: http://mocadev.tistory.com
+ * Github: http://github.com/chcjswo
+ */
+public class Question_15890 {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+
+        int yyyy = rnd.nextInt(9999);
+
+        System.out.println(yyyy);
+
+        int mm = rnd.nextInt(12);
+        
+        System.out.println(mm);
+
+        int dd = rnd.nextInt(31);
+    }
+}
