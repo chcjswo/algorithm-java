d267979055b8553c1c7f1b9c41caa88d20d1967e
tony <chcjswo@gmail.com>
2019-06-20 18:14:23 +0900

    백준 10718번 문제 풀이

---
+++ src/baekjoon/Question_10718.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+/**
+ * 백준 10718번 문제
+ * Created by chcjswo on 2019-06-20
+ * Blog: http://mocadev.tistory.com
+ * Github: http://github.com/chcjswo
+ */
+public class Question_10718 {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
