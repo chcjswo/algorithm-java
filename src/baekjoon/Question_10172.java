d4b879476d99ab285816d087247366eed419a1aa
tony <chcjswo@gmail.com>
2019-06-20 13:04:26 +0900

    백준 10172번 문제 풀이중

---
+++ src/baekjoon/Question_10172.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+/**
+ * 백준 10172번 문제
+ * Created by chcjswo on 2019-06-20
+ * Blog: http://mocadev.tistory.com
+ * Github: http://github.com/chcjswo
+ */
+public class Question_10172 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\__|");
+    }
+}
