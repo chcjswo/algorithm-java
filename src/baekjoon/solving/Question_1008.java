d2c6b9354dee72234b26274c44e822081e6bc506
tony <chcjswo@gmail.com>
2019-06-21 00:43:40 +0900

    백준 1008번 문제 풀이

---
+++ src/baekjoon/solving/Question_1008.java
@@ -0,0 +1,20 @@
+package baekjoon.solving;
+
+import java.util.Scanner;
+
+/**
+ * 백준 1008번 문제
+ * Created by chcjswo on 2019-06-21
+ * Blog: http://mocadev.tistory.com
+ * Github: http://github.com/chcjswo
+ */
+public class Question_1008 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        double a = in.nextDouble();
+        double b = in.nextDouble();
+
+        System.out.println(a / b);
+    }
+}
