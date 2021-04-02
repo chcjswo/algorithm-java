baf44e777fa0e5b9e51d2a0dcbf0179e53e2eb23
mc.jeon <mc.jeon@petdoc.co.kr>
2021-04-02 18:35:34 +0900

    백준 1920번 문제 풀이
    - 미해결
    - 이분탐색으로 풀 것

---
+++ src/main/java/baekjoon/Q_1920.java
@@ -0,0 +1,38 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q_1920 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int[] a = new int[n1];
+        for (int i = 0; i < n1; i++) {
+            a[i] = sc.nextInt();
+        }
+        int n2 = sc.nextInt();
+        int[] b = new int[n2];
+        for (int i = 0; i < n2; i++) {
+            b[i] = sc.nextInt();
+        }
+        int[] answer = new int[n2];
+        Arrays.fill(answer, 0);
+
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < n2; j++) {
+                if (a[i] == b[j]) {
+                    answer[j] = 1;
+                    break;
+                }
+            }
+        }
+
+        for (int i : answer) {
+            System.out.println(i);
+        }
+
+    }
+
+}
