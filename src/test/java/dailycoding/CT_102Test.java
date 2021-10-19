aaf1f3b585c781db113f7e3447e01b91cc24c154
mc.jeon <mc.jeon@petdoc.co.kr>
2021-10-19 20:39:56 +0900

    코드 정리

--- src/test/java/dailycoding/CT_102Test.java
+++ src/test/java/dailycoding/CT_102Test.java
@@ -2,8 +2,7 @@ package dailycoding;
 
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Created by chcjswo on 2020-06-30
@@ -13,14 +12,14 @@ import static org.hamcrest.MatcherAssert.assertThat;
 class CT_102Test {
 
     @Test
-    public void ct102Test() {
+    void ct102Test() {
         CT_102 ct_102 = new CT_102();
 
 //        assertThat(ct_102.getAlpha(1), is("A"));
 //        assertThat(ct_102.getAlpha(26), is("Z"));
 //        assertThat(ct_102.getAlpha(27), is("AA"));
 //        assertThat(ct_102.getAlpha(106), is("DB"));
-        assertThat(ct_102.getAlpha(706), is("AAD"));
+        assertEquals(ct_102.getAlpha(706), "AAD");
     }
 
 }
