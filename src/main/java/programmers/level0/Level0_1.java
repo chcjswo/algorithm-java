e8e2e1d9ae1436c8fa07ccd3d072fb5509f41b33
chcjswo <chcjswo@gmail.com>
2022-11-15 18:13:44 +0900

    프로그래머스 레벨0 - 옹알이(1) 문제 풀이

---
+++ src/main/java/programmers/level0/Level0_1.java
@@ -0,0 +1,28 @@
+package programmers.level0;
+
+/**
+ * 프로그래머스 - 레벨0 - 옹알이(1)
+ *
+ * @author mcjeon
+ * @version 1.0.0
+ * @since 2022-11-15
+ **/
+public class Level0_1 {
+    public static void main(String[] args) {
+        Level0_1 level01 = new Level0_1();
+//        String[] s = {"aya", "yee", "u", "maa", "wyeoo"};
+        String[] s = {"ayaye", "uuuma", "ye", "yemawoo", "ayaa"};
+        int solution = level01.solution(s);
+        System.out.println("solution = " + solution);
+    }
+
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for (String s : babbling) {
+            if (s.matches("(aya|ye|woo|ma)+")) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
