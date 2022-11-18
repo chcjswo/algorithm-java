99f6077b396aa77170863004a1f8cea1330d39f3
chcjswo <chcjswo@gmail.com>
2022-11-18 14:43:44 +0900

    프로그래머스 레벨0 - 배열의 평균값 풀이

---
+++ src/main/java/programmers/level0/P_7.java
@@ -0,0 +1,23 @@
+package programmers.level0;
+
+import java.util.Arrays;
+
+/**
+ * 배열의 평균값
+ *
+ * @author mcjeon
+ * @version 1.0.0
+ * @since 2022-11-18
+ **/
+public class P_7 {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        double solution = solution(nums);
+        System.out.println("solution = " + solution);
+    }
+
+    private static double solution(int[] numbers) {
+        return Arrays.stream(numbers).average().orElse(0);
+    }
+}
