bd686e0834d9a7a597716e0afcd2949e3162d42a
tony <chcjswo@gmail.com>
2023-09-10 23:56:28 +0900

    백준 1260번 문제 풀이

--- src/main/java/baekjoon/dfs/Q1260.java
+++ src/main/java/baekjoon/dfs/Q1260.java
@@ -1,6 +1,7 @@
 package baekjoon.dfs;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
@@ -20,11 +21,59 @@ public class Q1260 {
 	static int m;
 	static int v;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		v = Integer.parseInt(st.nextToken());
+
+		graph = new boolean[MAX][MAX];
+		visited = new boolean[MAX];
+		queue = new ArrayList<>();
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			graph[x][y] = graph[y][x] = true;
+		}
+
+		dfs(v);
+		System.out.println();
+
+		br.close();
+		bfs();
+	}
+
+	private static void bfs() {
+		queue = new ArrayList<>();
+		visited = new boolean[MAX];
+
+		queue.add(v);
+		visited[v] = true;
+
+		while (!queue.isEmpty()) {
+			int index = queue.remove(0);
+			System.out.print(index + " ");
+
+			for (int i = 1; i <= n; i++) {
+				if (graph[index][i] && !visited[i]) {
+					queue.add(i);
+					visited[i] = true;
+				}
+			}
+		}
+	}
+
+	private static void dfs(int index) {
+		visited[index] = true;
+		System.out.print(index + " ");
+
+		for (int i = 1; i <= n; i++) {
+			if (graph[index][i] && !visited[i]) {
+				dfs(i);
+			}
+		}
 	}
 }
